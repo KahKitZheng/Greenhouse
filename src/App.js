81d4fe5148982af225d5f6829c184f58d463ae9c
Kah Kit Zheng <kahkitzheng@gmail.com>
2020-08-19 16:51:24 +0200

    Feature: Add tailwind CSS to project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Greenhouse Application</p>
-      </header>
+      <div className="h-full w-full bg-gray-800 flex items-center justify-center flex-col">
+        <p className="text-white font-black text-5xl">Greenhouse Application</p>
+        <p className="text-gray-400 font-medium text-2xl">
+          A react application
+        </p>
+      </div>
     </div>
   );
 }
