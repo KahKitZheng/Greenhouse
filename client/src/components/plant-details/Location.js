78b0497cb6ee15651bc5bb6b4dfade542035c947
Kah Kit Zheng <Kahkitzheng@gmail.com>
2020-08-25 16:17:47 +0200

    Fix: Don't show location section if no data is present

--- client/src/components/plant-details/Location.js
+++ client/src/components/plant-details/Location.js
@@ -2,22 +2,28 @@ import React from "react";
 
 const PlantLocation = ({ observation, distribution }) => {
   return (
-    <div className="mb-8 md:mb-12">
-      <p className="text-2xl font-bold uppercase">Locations</p>
-      <hr className="border-2 border-gray-300 mb-4 md:mt-2" />
-      <div>
-        <div>
-          <p className="font-bold">Observation:&nbsp;</p>
-          <p>{observation !== null ? observation : "N/A"}</p>
+    observation !== undefined &&
+    distribution !==
+      undefined(
+        <div className="mb-8 md:mb-12">
+          <p className="text-2xl font-bold uppercase">Locations</p>
+          <hr className="border-2 border-gray-300 mb-4 md:mt-2" />
+          <div>
+            <div>
+              <p className="font-bold">Observation:&nbsp;</p>
+              <p>{observation !== null ? observation : "N/A"}</p>
+            </div>
+            {distribution && (
+              <ul className="grid grid-cols-2 md:grid-cols-3 gap-x-4">
+                {distribution.native &&
+                  distribution.native.map((place, index) => (
+                    <li key={index}>{place}</li>
+                  ))}
+              </ul>
+            )}
+          </div>
         </div>
-        <ul className="grid grid-cols-2 md:grid-cols-3 gap-x-4">
-          {distribution &&
-            distribution.native.map((place, index) => (
-              <li key={index}>{place}</li>
-            ))}
-        </ul>
-      </div>
-    </div>
+      )
   );
 };
 
